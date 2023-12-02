f10e76ceefd0b1fba4d3176ad7256fb15da099cf
Khaled Tayem <81522465+22-khaled@users.noreply.github.com>
2023-12-02 16:11:39 +0300

    Update Function.py

--- Function.py
+++ Function.py
@@ -6,16 +6,7 @@ from pages.Khaled import budget_app2
 from pages.Leen import budget_app3
 from pages.Sondos import budget_app4
 from pages.Husam import budget_app5
-# Everything is accessible via the st.secrets dict:
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 
-# And the root-level secrets are also accessible as environment variables:
-st.write(
-    "Has environment variables been set:",
-    os.environ["db_username"] == st.secrets["db_username"],
-)
 
 def main():
     st.title("My Streamlit App")
