b6f39cf9bcbb60f1fff1b6653b9313b4b11d969e
Khaled Tayem <81522465+22-khaled@users.noreply.github.com>
2023-12-02 15:10:16 +0300

    Update Sondos.py

--- pages/Sondos.py
+++ pages/Sondos.py
@@ -3,70 +3,70 @@ import pandas as pd
 from datetime import datetime, timedelta
 from google.cloud import firestore
 
-def budget_app2():
+def budget_app4():
     st.title("Budget Tracker App")
     st.divider()
 
-    st.title('Khaled payments: ')
+    st.title('Sondos payments: ')
     st.divider()
-    start_date2 = datetime(2023, 12, 1)
-    end_date2 = datetime(2023, 12, 31)
+    start_date4 = datetime(2023, 12, 1)
+    end_date4 = datetime(2023, 12, 31)
 
     # Create a list of days for December
-    day_list2 = [start_date2 + timedelta(days=x) for x in range((end_date2 - start_date2).days + 1)]
+    day_list4 = [start_date4 + timedelta(days=x) for x in range((end_date4 - start_date4).days + 1)]
 
     # Create a DataFrame with columns 'Date', 'Day', and 'Day_Name'
-    df2 = pd.DataFrame({'Date': day_list2, 'Day': [d.day for d in day_list2], 'Day_Name': [d.strftime('%A') for d in day_list2]})
+    df4 = pd.DataFrame({'Date': day_list4, 'Day': [d.day for d in day_list4], 'Day_Name': [d.strftime('%A') for d in day_list4]})
 
     # Create a list of dates for December
-    date_list2 = [start_date2 + timedelta(days=x) for x in range((end_date2 - start_date2).days + 1)]
+    date_list4 = [start_date4 + timedelta(days=x) for x in range((end_date4 - start_date4).days + 1)]
 
-    if ("is_initialized2" not in st.session_state) or not st.session_state["is_initialized2"]:
+    if ("is_initialized4" not in st.session_state) or not st.session_state["is_initialized4"]:
         # Created a data table
-        _data2 = pd.DataFrame({
-            "Days": df2['Day_Name'],
-            "Date":date_list2,
-            "Payment":[0] * len(day_list2)
+        _data4 = pd.DataFrame({
+            "Days": df4['Day_Name'],
+            "Date":date_list4,
+            "Payment":[0] * len(day_list4)
         })
 
         # Setting state variables
-        st.session_state["data2"] = _data2
-        st.session_state["is_edit_mode2"] = False
-        st.session_state["edit_button_label2"] = "Edit2"
+        st.session_state["data4"] = _data4
+        st.session_state["is_edit_mode4"] = False
+        st.session_state["edit_button_label4"] = "Edit4"
 
-        st.session_state["is_initialized2"] = True
+        st.session_state["is_initialized4"] = True
     def toggle_edit_mode(enabled: bool):
-        st.session_state["is_edit_mode2"] = enabled
+        st.session_state["is_edit_mode4"] = enabled
 
         if enabled:
-            st.session_state["edit_button_label2"] = "Editing2..."
+            st.session_state["edit_button_label4"] = "Editing4..."
         else:
-            st.session_state["edit_button_label2"] = "Edit2"
+            st.session_state["edit_button_label4"] = "Edit4"
 
     # Callbacks
     def on_reset_button_click():
-        st.session_state["is_initialized2"] = False
+        st.session_state["is_initialized4"] = False
 
     def on_edit_button_click():
         toggle_edit_mode(True)
 
-    def on_save_button_click(new_data2: pd.DataFrame):
+    def on_save_button_click(new_data4: pd.DataFrame):
         toggle_edit_mode(False)
-        st.session_state["data2"] = new_data2
+        st.session_state["data4"] = new_data4
 
     def on_cancel_button_click():
         toggle_edit_mode(False)
 
 
-    st.button(st.session_state["edit_button_label2"],
+    st.button(st.session_state["edit_button_label4"],
             on_click=on_edit_button_click,
-            disabled=st.session_state["is_edit_mode2"])
+            disabled=st.session_state["is_edit_mode4"])
 
 
     # State-based layout
-    if st.session_state["is_edit_mode2"]:
+    if st.session_state["is_edit_mode4"]:
         # Edit mode layout
-        new_data2 = st.data_editor(st.session_state["data2"],
+        new_data4 = st.data_editor(st.session_state["data4"],
                                 disabled=["Days", "Date"],
                                 use_container_width=True)
 
@@ -85,34 +85,34 @@ def budget_app2():
 
         cols = st.columns(2)
         with cols[0]:
-            st.button("Cancel2", type="secondary", on_click=on_cancel_button_click)
+            st.button("Cancel4", type="secondary", on_click=on_cancel_button_click)
         with cols[1]:
-            st.button("Save2", type="primary", on_click=on_save_button_click, args=(new_data2, ))
+            st.button("Save4", type="primary", on_click=on_save_button_click, args=(new_data4, ))
     else:
         # Viewing mode layout
-        st.table(st.session_state["data2"])
-        gpa = st.session_state["data2"]["Payment"].sum()
+        st.table(st.session_state["data4"])
+        gpa = st.session_state["data4"]["Payment"].sum()
         st.write("Counted payments:", f'{gpa}')
 
         st.button("Reset", on_click=on_reset_button_click)
 
     # Display GPA
-    db2 = firestore.Client.from_service_account_json("budjet-7a5ac-firebase-adminsdk-iohd5-9a378ad90d.json")
+    db4 = firestore.Client.from_service_account_json("budjet-7a5ac-firebase-adminsdk-iohd5-9a378ad90d.json")
     if st.button("Save to Firestore", key='save_firestore'):
         # Assuming _data5 is a Pandas DataFrame
-        if "data2" not in st.session_state:
+        if "data4" not in st.session_state:
             st.error("Data not found. Please load or create the data first.")
         else:
             # Create a reference to the Firestore collection
-            collection_ref = db2.collection("Payment")
+            collection_ref = db4.collection("Payment")
 
             # Iterate over the DataFrame and add each record to Firestore
             done_flag = False  # Initialize the flag
 
-            for _, row in st.session_state["data2"].iterrows():
+            for _, row in st.session_state["data4"].iterrows():
                 record = row.to_dict()
                 doc_ref = collection_ref.add(record)[1]
-                edited_value2 = record.get('fieldName')  # Adjust 'fieldName' to the actual field name in your document
+                edited_value4 = record.get('fieldName')  # Adjust 'fieldName' to the actual field name in your document
 
                 # Optionally, retrieve and display the last added document
                 if "doc_ref" in locals():
@@ -125,6 +125,5 @@ def budget_app2():
             # Display 'Done...' only once
             if done_flag:
                 st.write('Done...')
-                st.write(f"Edited Value: {edited_value2}")
-
+                st.write(f"Edited Value: {edited_value4}")
 
